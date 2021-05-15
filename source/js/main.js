a4a35f6012e6c7194ff6043033cec15e74c18380
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-05-15 14:45:34 +0300

    CSS of index.html

---
+++ source/js/main.js
@@ -0,0 +1,9 @@
+"use strict"
+
+//navigation
+
+const navigationButton = document.querySelector(".navigation__toggle");
+const navigationCartMobile = document.querySelector(".navigation__cart-mobile");
+
+navigationButton.classList.add("hidden");
+navigationCartMobile.classList.add("hidden");
